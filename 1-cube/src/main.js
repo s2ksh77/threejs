aeb50a0afa14f480f1d53307b62481b36c2fff1b
soohyun <soohyun_kim2@tmax.co.kr>
2023-06-28 13:22:57 +0900

    feat: scene, camera, cube setting

--- 1-cube/src/main.js
+++ 1-cube/src/main.js
@@ -5,5 +5,35 @@ window.addEventListener("load", () => {
 });
 
 function init() {
-  console.log(THREE);
+  const renderer = new THREE.WebGLRenderer({
+    // alpha: true,
+    antialias: true,
+  });
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  document.body.appendChild(renderer.domElement);
+
+  const scene = new THREE.Scene();
+
+  // 물체에 대한 원근감 표현 카메라
+  const camera = new THREE.PerspectiveCamera(
+    75, // fov 카메라의 각도
+    window.innerWidth / window.innerHeight, //
+    1, // near
+    500 // far 성능적인 문제 때문에 범위를 적어준다.
+  );
+
+  const geometry = new THREE.BoxGeometry(2, 2, 2);
+  const material = new THREE.MeshBasicMaterial({ color: "#993366" });
+
+  const cube = new THREE.Mesh(geometry, material);
+
+  scene.add(cube);
+
+  camera.position.set(3, 4, 5);
+
+  camera.lookAt(cube.position);
+
+  renderer.render(scene, camera);
 }
