94b798fc196266b29abf96c9521b4692ed86091e
soohyun <soohyun_kim2@tmax.co.kr>
2023-06-28 23:12:43 +0900

    fix:boilerplate

--- 0-boilerplate/src/main.js
+++ 0-boilerplate/src/main.js
@@ -1,9 +1,47 @@
-import * as THREE from "three";
+import * as THREE from 'three';
 
-window.addEventListener("load", () => {
+window.addEventListener('load', () => {
   init();
 });
 
 function init() {
-  console.log(THREE);
+  const renderer = new THREE.WebGLRenderer({
+    antialias: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  document.body.appendChild(renderer.domElement);
+
+  const scene = new THREE.Scene();
+
+  // 물체에 대한 원근감 표현 카메라
+  const camera = new THREE.PerspectiveCamera(
+    75, // fov 카메라의 각도
+    window.innerWidth / window.innerHeight, //
+    1, // near
+    500 // far 성능적인 문제 때문에 범위를 적어준다.
+  );
+
+  camera.position.z = 5;
+
+  render();
+
+  function render() {
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
+  }
+
+  function handleResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    renderer.render(scene, camera);
+  }
+  window.addEventListener('resize', () => {
+    handleResize();
+  });
 }
