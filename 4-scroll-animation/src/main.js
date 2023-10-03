5d2fdb544aa2c7b029f430f4a775ad5912f32bdd
soohyun <s2ksh_77@naver.com>
2023-10-03 19:10:42 +0900

    feat: 3d models added

--- 4-scroll-animation/src/main.js
+++ 4-scroll-animation/src/main.js
@@ -1,11 +1,12 @@
 import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import GUI from "lil-gui";
 
 window.addEventListener("load", () => {
   init();
 });
 
-function init() {
+async function init() {
   const gui = new GUI();
   const canvas = document.querySelector("#canvas");
 
@@ -82,6 +83,24 @@ function init() {
 
   scene.add(wave);
 
+  const gltfLoader = new GLTFLoader();
+
+  const gltf = await gltfLoader.loadAsync("./models/ship/scene.gltf");
+
+  const ship = gltf.scene;
+
+  ship.update = function () {
+    const elapsedTime = clock.getElapsedTime();
+
+    ship.position.y = Math.sin(elapsedTime * 3);
+  };
+
+  ship.rotation.y = Math.PI;
+
+  ship.scale.set(40, 40, 40);
+
+  scene.add(ship);
+
   const pointLight = new THREE.PointLight(0xffffff, 1);
 
   pointLight.position.set(15, 15, 15);
@@ -98,6 +117,9 @@ function init() {
 
   function render() {
     wave.update();
+    ship.update();
+
+    camera.lookAt(ship.position);
 
     renderer.render(scene, camera);
 
