70bd2b4dfa0efac546801e2a364f042d2102b2cc
soohyun <soohyun_kim2@tmax.co.kr>
2023-10-06 16:16:26 +0900

    feat: "random paticle color"

--- 5-fireworks/src/main.js
+++ 5-fireworks/src/main.js
@@ -30,22 +30,37 @@ function init() {
   // const geometry = new THREE.SphereGeometry();
   const geometry = new THREE.BufferGeometry();
 
-  const count = 10000;
+  const count = 1000;
 
   const positions = new Float32Array(count * 3);
+  const colors = new Float32Array(count * 3);
 
   for (let i = 0; i < count; i++) {
     positions[i * 3] = THREE.MathUtils.randFloatSpread(10); // x
     positions[i * 3 + 1] = THREE.MathUtils.randFloatSpread(10); // y
     positions[i * 3 + 2] = THREE.MathUtils.randFloatSpread(10); // z
+
+    colors[i * 3] = Math.random();
+    colors[i * 3 + 1] = Math.random();
+    colors[i * 3 + 2] = Math.random();
   }
 
   geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+  geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
 
   const meterial = new THREE.PointsMaterial({
     color: 0xccaaff,
-    size: 0.01,
+    size: 0.1,
+    vertexColors: true, // 각 정점마다 랜덤한 색상을 하겠다
   });
+
+  const textureLoader = new THREE.TextureLoader();
+
+  const texture = textureLoader.load('./assets/textures/particle.png');
+
+  meterial.alphaMap = texture;
+  meterial.transparent = true; // texture 입히고 배경을 안보이게
+  meterial.depthWrite = false;
 
   const points = new THREE.Points(geometry, meterial);
 
