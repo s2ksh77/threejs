2dbda88b7cdcf2b3445e238b5f5f1fe9ffd9fce7
soohyun <soohyun_kim2@tmax.co.kr>
2023-10-08 16:11:20 +0900

    feat: 3d textures another way added

--- 6-skybox/src/main.js
+++ 6-skybox/src/main.js
@@ -20,37 +20,70 @@ function init() {
     75, // fov 카메라의 각도
     window.innerWidth / window.innerHeight, //
     1, // near
-    10000 // far 성능적인 문제 때문에 범위를 적어준다.
+    500 // far 성능적인 문제 때문에 범위를 적어준다.
   );
 
   camera.position.z = 5;
 
+  /** 큐브맵 텍스쳐를 이용한 3차원 공간 */
+  // const controls = new OrbitControls(camera, renderer.domElement);
+
+  // controls.minDistance = 5;
+  // controls.maxDistance = 100;
+
+  // const textureLoader = new THREE.TextureLoader().setPath('assets/textures/Yokohama/');
+
+  // const images = ['posx.jpg', 'negx.jpg', 'posy.jpg', 'negy.jpg', 'posz.jpg', 'negz.jpg'];
+
+  //   THREE.CubeTexture
+
+  // const boxGeometry = new THREE.BoxGeometry(5000, 5000, 5000);
+
+  // const meterials = images.map(
+  //   (image) =>
+  //     new THREE.MeshBasicMaterial({
+  //       map: textureLoader.load(image),
+  //       side: THREE.BackSide,
+  //     })
+  // );
+
+  // const skybox = new THREE.Mesh(boxGeometry, meterials);
+
+  // scene.add(skybox);
+
+  /** 큐브맵 텍스쳐 로더를 이용한 3차원 공간 2 */
+  // new OrbitControls(camera, renderer.domElement);
+
+  // const cubeTextureLoader = new THREE.CubeTextureLoader().setPath(
+  //   'assets/textures/Yokohama/'
+  // );
+
+  // const images = ['posx.jpg', 'negx.jpg', 'posy.jpg', 'negy.jpg', 'posz.jpg', 'negz.jpg'];
+
+  // const cubeTextures = cubeTextureLoader.load(images);
+
+  // scene.background = cubeTextures;
+
+  /** 360 파노라마 텍스쳐 로더를 이용한 3차원 공간 3 */
   const controls = new OrbitControls(camera, renderer.domElement);
 
-  controls.minDistance = 5;
-  controls.maxDistance = 100;
+  controls.enableZoom = false;
+  controls.enableDamping = true;
+  controls.autoRotate = true;
+  controls.autoRotateSpeed = 0.5;
 
-  const textureLoader = new THREE.TextureLoader().setPath('assets/textures/Yokohama/');
+  const textureLoader = new THREE.TextureLoader();
 
-  const images = ['posx.jpg', 'negx.jpg', 'posy.jpg', 'negy.jpg', 'posz.jpg', 'negz.jpg'];
+  const texture = textureLoader.load('assets/textures/village.jpeg');
 
-  const boxGeometry = new THREE.BoxGeometry(5000, 5000, 5000);
+  texture.mapping = THREE.EquirectangularReflectionMapping;
 
-  const meterials = images.map(
-    (image) =>
-      new THREE.MeshBasicMaterial({
-        map: textureLoader.load(image),
-        side: THREE.BackSide,
-      })
-  );
-
-  const skybox = new THREE.Mesh(boxGeometry, meterials);
-
-  scene.add(skybox);
+  scene.background = texture;
 
   render();
 
   function render() {
+    controls.update();
     renderer.render(scene, camera);
 
     requestAnimationFrame(render);
